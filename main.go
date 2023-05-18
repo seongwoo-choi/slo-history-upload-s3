ab3a7b440fede48cfca6566dab9f8654b856c19e
최성우/SRE팀 <seongwoo.choi@gmail.com>
2023-05-18 15:53:47 +0900

    리팩터

--- main.go
+++ main.go
@@ -4,82 +4,46 @@ package main
 // 퀵사이트에서 s3 를 바로 볼 수 도 있음.
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"os"
 	"strconv"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadog"
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
-func CreateJSONFile(responseContent []byte) error {
-	f, fileCreateErr := os.Create("slo_history.json")
-	if fileCreateErr != nil {
-		fmt.Println(fileCreateErr)
-		return fileCreateErr
-	}
-
-	defer f.Close()
-
-	_, fileWriteErr := f.Write(responseContent)
-	if fileWriteErr != nil {
-		fmt.Println(fileWriteErr)
-		return fileWriteErr
-	}
-
-	return nil
-}
-
-func UploadS3() error {
-	sess, err := session.NewSession(&aws.Config{
+func FileUploadS3(responseContent []byte) error {
+	sess, _ := session.NewSession(&aws.Config{
 		Region: aws.String("ap-northeast-2"),
 	})
-	if err != nil {
-		log.Fatal("Failed to create AWS session:", err)
-	}
 
-	svc := s3.New(sess)
-
-	file, err := os.Open("slo_history.json")
-	if err != nil {
-		return err
-	}
-	defer file.Close()
+	uploader := s3manager.NewUploader(sess)
 
 	today := time.Now().Format("2006-01-02")
-	bucket := os.Getenv("BUCKET")
-	key := os.Getenv("KEY")
 
-	key = fmt.Sprintf("%s/%s/slo_history.json", key, today)
+	sloHistoryReader := bytes.NewReader(responseContent)
 
-	putObjectInput := &s3.PutObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
-		Body:   file,
-		//ContentType: aws.String(fileType),
-	}
+	_, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(os.Getenv("BUCKET")),
+		Key:    aws.String(fmt.Sprintf("%s/%s/slo_history.json", os.Getenv("KEY"), today)),
+		Body:   sloHistoryReader,
+	})
 
-	_, err = svc.PutObject(putObjectInput)
 	if err != nil {
-		fmt.Println(err)
 		return err
 	}
-
-	fmt.Println("파일이 업로드되었습니다.")
 
 	return nil
 }
 
 func GetSloHistory(sloId string, day int) error {
-	value := make(map[string]interface{})
 	ctx := datadog.NewDefaultContext(context.Background())
 	configuration := datadog.NewConfiguration()
 	apiClient := datadog.NewAPIClient(configuration)
@@ -93,27 +57,13 @@ func GetSloHistory(sloId string, day int) error {
 	}
 
 	responseContent, _ := json.MarshalIndent(resp, "", "  ")
-	//fmt.Fprintf(os.Stdout, "Response from `ServiceLevelObjectivesApi.GetSLOHistory`:\n%s\n", responseContent)
 
-	err = json.Unmarshal(responseContent, &value)
+	err = FileUploadS3(responseContent)
 	if err != nil {
 		return err
 	}
 
-	err = CreateJSONFile(responseContent)
-	if err != nil {
-		return err
-	}
-
-	//data := value["data"].(map[string]interface{})
-	//monitors := data["monitors"].([]interface{})
-	//overall := data["overall"].(map[string]interface{})
-	//sliValue := overall["sli_value"].(float64)
-
-	err = UploadS3()
-	if err != nil {
-		return err
-	}
+	fmt.Println("파일 업로드 완료.")
 
 	return nil
 }

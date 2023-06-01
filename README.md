c1c14eddada91de58bb658c49dc0d2be33e84542
최성우/SRE팀 <seongwoo.choi@gmail.com>
2023-06-01 16:42:49 +0900

    SES를 통해 이메일 전송 및 README 파일 수정

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# slo-history-upload-s3
+### slo-history-upload-s3
+
+#### 환경 변수
+
+- BUCKET
+  - S3 버킷 이름
+    - ex) my_bucket
+- KEY
+  - S3 버킷에 저장할 디렉토리 경로입니다.
+    - ex) slo_metric/example_slo
+- DAY
+  - 현재 날짜에서 7일 전의 SLO History 내역을 가져옵니다.
+    - ex) -7, -30, -90
+- DD_SITE
+  - datadog 사이트를 지정합니다.
+    - ex) datadoghq.com
+- SLO_ID
+  - SLO History 를 가져오려는 SLO_ID 입니다.
+    - ex) slo_id_123456789
+- DD_API_KEY
+  - datadog API 키입니다.
+    - ex) 1234567890abcdefg
+- DD_APP_KEY
+  - datadog APP 키입니다.
+    - ex) 1234567890abcdefg
+- RECIPIENT
+  - SLO 대시보드 링크를 전송할 이메일 주소입니다.
+    - ex) "test1@example.com, test2@example.com, test3@example.com"
+
+
+#### 동작
+
+데이터독 SLO_ID 에 해당하는 SLO History 를 가져와서 S3 버킷에 저장합니다. 그 후, AWS SES 를 사용하여 특정 이메일에 데이터독 SLO 대시보드 링크를 전송합니다.
--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
-// s3 에 그냥 데이터 업로드 => 아테나 쿼리에서 테이블을 만드는데 s3 에서 데이터를 가져와서 컬럼을 만들 수 있다.
-// 퀵사이트에서 s3 를 바로 볼 수 도 있음.
-
 import (
 	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/aws/aws-sdk-go/service/ses"
 	"os"
 	"strconv"
 	"strings"
@@ -20,6 +18,92 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
+const (
+	Sender  = "noreply@musinsa.com"
+	CharSet = "UTF-8"
+)
+
+type SloHistory struct {
+	toTs     int64
+	sliValue float64
+}
+
+func getMonthRange(year int, month int) (time.Time, time.Time) {
+	firstDay := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+	lastDay := time.Date(year, time.Month(month+1), 1, 0, 0, 0, 0, time.UTC)
+	return firstDay, lastDay
+}
+
+func toMilliseconds(dt time.Time) int64 {
+	return dt.UnixNano() / int64(time.Millisecond)
+}
+
+func getMilliseconds(firstDay, lastDay time.Time) (int64, int64) {
+	firstDayMillis := toMilliseconds(firstDay)
+	lastDayMillis := toMilliseconds(lastDay)
+	return firstDayMillis, lastDayMillis
+}
+
+func GetYearMonthDay() (string, string, string) {
+	today := time.Now().Format("2006-01-02")
+	s := strings.Split(today, "-")
+	y := s[0]
+	m := s[1]
+	d := s[2]
+
+	return y, m, d
+}
+
+func SendMail(sloId string) error {
+	y, m, _ := GetYearMonthDay()
+
+	iY, _ := strconv.Atoi(y)
+	iM, _ := strconv.Atoi(m)
+	firstDay, lastDay := getMonthRange(iY, iM)
+	monthNumber := int(firstDay.Month())
+	firstDayMillisecs, lastDayMillisecs := getMilliseconds(firstDay, lastDay)
+	dashBoardURL := fmt.Sprintf("https://app.datadoghq.com/dashboard/%v/slo-slo?from_ts=%v&to_ts=%v&live=true", sloId, firstDayMillisecs, lastDayMillisecs)
+
+	recipients := strings.Split(os.Getenv("RECIPIENT"), ",")
+	subject := fmt.Sprintf("%v월 무신사 플랫폼서비스 SLO 요약 리포트", monthNumber)
+	htmlBody := fmt.Sprintf("<h1>%v월 무신사 플랫폼 서비스 SLO 요약 리포트</h1>", monthNumber) + fmt.Sprintf("<h2><a href=%v>무신사 플랫폼 서비스 SLO 요약 대시보드입니다.</a></h2>", dashBoardURL)
+
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("ap-northeast-2"),
+	})
+
+	svc := ses.New(sess)
+
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			CcAddresses: []*string{},
+			ToAddresses: aws.StringSlice(recipients),
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Charset: aws.String(CharSet),
+					Data:    aws.String(htmlBody),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String(CharSet),
+				Data:    aws.String(subject),
+			},
+		},
+		Source: aws.String(Sender),
+	}
+
+	_, err := svc.SendEmail(input)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("send email success")
+
+	return nil
+}
+
 func FileUploadS3(responseContent []byte) error {
 	sess, _ := session.NewSession(&aws.Config{
 		Region: aws.String("ap-northeast-2"),
@@ -27,11 +111,7 @@ func FileUploadS3(responseContent []byte) error {
 
 	uploader := s3manager.NewUploader(sess)
 
-	today := time.Now().Format("2006-01-02")
-	s := strings.Split(today, "-")
-	y := s[0]
-	m := s[1]
-	d := s[2]
+	y, m, d := GetYearMonthDay()
 
 	sloHistoryReader := bytes.NewReader(responseContent)
 
@@ -48,7 +128,9 @@ func FileUploadS3(responseContent []byte) error {
 	return nil
 }
 
-func GetSloHistory(sloId string, day int) error {
+func GetSloHistory(sloId string, day int) (SloHistory, error) {
+	var sloHistory SloHistory
+
 	ctx := datadog.NewDefaultContext(context.Background())
 	configuration := datadog.NewConfiguration()
 	apiClient := datadog.NewAPIClient(configuration)
@@ -58,7 +140,7 @@ func GetSloHistory(sloId string, day int) error {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error when calling `ServiceLevelObjectivesApi.GetSLOHistory`: %v\n", err)
 		fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
-		return err
+		return sloHistory, err
 	}
 
 	responseContent, _ := json.MarshalIndent(resp, "", "  ")
@@ -67,20 +149,28 @@ func GetSloHistory(sloId string, day int) error {
 
 	err = FileUploadS3(responseContent)
 	if err != nil {
-		return err
+		return sloHistory, err
 	}
 
 	fmt.Println("파일 업로드 완료.")
 
-	return nil
+	//overall := data["overall"].(map[string]interface{})
+	//sliValue := overall["sli_value"].(float64)
+
+	//sloHistory.toTs = responseContent[""]
+
+	return sloHistory, err
 }
 
 func main() {
 	// SLO ID 환경 변수 처리
 	sloId := os.Getenv("SLO_ID")
 	day, _ := strconv.Atoi(os.Getenv("DAY"))
-	err := GetSloHistory(sloId, day)
-	if err != nil {
+	if _, err := GetSloHistory(sloId, day); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := SendMail(sloId); err != nil {
 		fmt.Println(err)
 	}
 }
